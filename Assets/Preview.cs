4ede97a54fd99c2e07abf0a89e62e698ca8a4dc4
Jorge Acosta <transmigrado@gmail.com>
2020-02-18 08:03:22 -0300

    add Preview card

---
+++ Assets/Preview.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preview : MonoBehaviour
+{
+    void Start()
+    {
+        open();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void open()
+    {
+        Hashtable hashtable = new Hashtable();
+        hashtable.Add("x",  0.0f);
+        hashtable.Add("time", 1.0f);
+        hashtable.Add("easytype", iTween.EaseType.easeInBack);
+        iTween.MoveTo(this.gameObject, hashtable);
+    }
+}
